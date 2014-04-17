19c04d6deacdd26432fdc24e25828454d122684e
pluto.bing.liu <pluto.bing.liu@gmail.com>
2014-04-17 22:22:27 +0800

    实现异常抛出配置处理器

--- alpaca/alpaca-core/src/main/java/net/vdrinkup/alpaca/configuration/processor/ThrowExceptionProcessor.java
+++ alpaca/alpaca-core/src/main/java/net/vdrinkup/alpaca/configuration/processor/ThrowExceptionProcessor.java
@@ -9,12 +9,17 @@ import net.vdrinkup.alpaca.configuration.model.ThrowExceptionDefinition;
 import net.vdrinkup.alpaca.context.ContextStatus;
 import net.vdrinkup.alpaca.context.DataContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 异常抛出配置处理器
  * @author pluto.bing.liu
  */
 public class ThrowExceptionProcessor extends AbstractProcessor< ThrowExceptionDefinition > {
 
+	private static Logger LOG = LoggerFactory.getLogger( ThrowExceptionProcessor.class );
+	
 	public ThrowExceptionProcessor( ThrowExceptionDefinition t ) {
 		super( t );
 	}
@@ -32,6 +37,9 @@ public class ThrowExceptionProcessor extends AbstractProcessor< ThrowExceptionDe
 			exception = e;
 		} catch ( ClassNotFoundException e ) {
 			exception = e;
+		}
+		if ( LOG.isDebugEnabled() ) {
+			LOG.debug( exception.getMessage(), exception );
 		}
 		context.setException( exception );
 		context.setStatus( ContextStatus.EXCEPTION );
